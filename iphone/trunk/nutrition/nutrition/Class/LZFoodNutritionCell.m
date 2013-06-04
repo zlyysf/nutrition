8faf559d231dc34abf03064a1738373492f12b3a
mliu <mliu@tagged.com>
2013-06-04 16:37:17 +0800

    add click effect.

--- iphone/trunk/nutrition/nutrition/Class/LZFoodNutritionCell.m
+++ iphone/trunk/nutrition/nutrition/Class/LZFoodNutritionCell.m
@@ -38,13 +38,13 @@
     {
         [self.supplyPercentlabel setTextColor:[UIColor whiteColor]];
         [self.nutritionNameLabel setTextColor:[UIColor whiteColor]];
-        //[self setBackgroundColor:[UIColor greenColor]];
+        [self setBackgroundColor:[UIColor colorWithRed:15/255.f green:148/255.f blue:26/255.f alpha:1.f]];
     }
     else
     {
         [self.supplyPercentlabel setTextColor:[UIColor blackColor]];
         [self.nutritionNameLabel setTextColor:[UIColor blackColor]];
-        //[self setBackgroundColor:[UIColor clearColor]];
+        [self setBackgroundColor:[UIColor clearColor]];
     }
 }
 
