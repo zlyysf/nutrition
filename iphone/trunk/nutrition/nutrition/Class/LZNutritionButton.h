30fb12f536c2f0136e6fefb79cf493d7b5359b6d
mliu <mliu@tagged.com>
2013-10-15 14:04:00 +0800

    change button UI.

---
+++ iphone/trunk/nutrition/nutrition/Class/LZNutritionButton.h
@@ -0,0 +1,13 @@
+//
+//  LZNutritionButton.h
+//  nutrition
+//
+//  Created by liu miao on 10/15/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZNutritionButton : UIButton
+
+@end
---
+++ iphone/trunk/nutrition/nutrition/Class/LZNutritionButton.m
@@ -0,0 +1,31 @@
+//
+//  LZNutritionButton.m
+//  nutrition
+//
+//  Created by liu miao on 10/15/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import "LZNutritionButton.h"
+
+@implementation LZNutritionButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
