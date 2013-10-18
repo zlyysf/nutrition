493ea7d08290554976d4bd3d055e88a83a458676
mliu <mliu@tagged.com>
2013-10-18 13:50:58 +0800

    change font size for food type name.

--- iphone/trunk/nutrition/nutrition/Class/LZFoodTypeButton.m
+++ iphone/trunk/nutrition/nutrition/Class/LZFoodTypeButton.m
@@ -7,20 +7,28 @@
 //
 
 #import "LZFoodTypeButton.h"
-
+#import "LZUtility.h"
 @implementation LZFoodTypeButton
 @synthesize alphaView,typeLabel;
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
+        float fontSize;
+        if ([LZUtility isCurrentLanguageChinese]) {
+            fontSize = 18;
+        }
+        else
+        {
+            fontSize = 16;
+        }
         // Initialization code
         self.alphaView = [[UIView alloc]initWithFrame:CGRectMake(1, frame.size.height-28-1, frame.size.width-2, 28)];
         [self.alphaView setBackgroundColor:[UIColor colorWithRed:0.f green:0.f blue:0.f alpha:0.5f]];
         [self addSubview:alphaView];
         self.typeLabel = [[UILabel alloc]initWithFrame:CGRectMake(1, frame.size.height-28-1, frame.size.width-2, 28)];
         [self.typeLabel setTextColor:[UIColor whiteColor]];
-        [self.typeLabel setFont:[UIFont boldSystemFontOfSize:18]];
+        [self.typeLabel setFont:[UIFont boldSystemFontOfSize:fontSize]];
         [self.typeLabel setTextAlignment:NSTextAlignmentCenter];
         [self.typeLabel setBackgroundColor:[UIColor clearColor]];
         [self addSubview:typeLabel];
