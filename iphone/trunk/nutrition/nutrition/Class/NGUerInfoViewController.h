8ac7d692cad439082f35d061e5c4f08317a59596
mliu <mliu@tagged.com>
2013-11-25 19:12:46 +0800

    change icon for user info view in tab bar.

---
+++ iphone/trunk/nutrition/nutrition/Class/NGUerInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  NGUerInfoViewController.h
+//  nutrition
+//
+//  Created by liu miao on 11/25/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGUerInfoViewController : UIViewController
+
+@end
---
+++ iphone/trunk/nutrition/nutrition/Class/NGUerInfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  NGUerInfoViewController.m
+//  nutrition
+//
+//  Created by liu miao on 11/25/13.
+//  Copyright (c) 2013 lingzhi mobile. All rights reserved.
+//
+
+#import "NGUerInfoViewController.h"
+
+@interface NGUerInfoViewController ()
+
+@end
+
+@implementation NGUerInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
