1ca719fb6b7a4c94a6f348be220195425bd812fc
Xinyu Wang <xwang@tagged.com>
2013-11-29 14:48:29 +0800

    add report page

--- android/NutritionFoodGuide/src/com/lingzhimobile/nutritionfoodguide/v3/activity/V3ActivityReport.java
+++ android/NutritionFoodGuide/src/com/lingzhimobile/nutritionfoodguide/v3/activity/V3ActivityReport.java
@@ -1,18 +1,31 @@
 package com.lingzhimobile.nutritionfoodguide.v3.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.lingzhimobile.nutritionfoodguide.R;
 import com.lingzhimobile.nutritionfoodguide.v3.fragment.RecommentFoodFragment;
 
 public class V3ActivityReport extends V3BaseActivity {
 
+    LinearLayout elementLinearLayout;
     LinearLayout recommendViewPager;
+    LinearLayout diseaseLinearLayout;
+    LinearLayout attentionLinearLayout;
+    
+    String[] elements;
+    List<Pair<String, String>> recommentFoods;
+    String[] diseases;
+    String[] attentions;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +36,52 @@ public class V3ActivityReport extends V3BaseActivity {
         title.setText("健康报告");
         rightButton.setVisibility(View.GONE);
         
-        String[] recommentFoods = new String[]{"鸡蛋","开心果","生蚝","西兰花","鸡蛋","开心果","生蚝","西兰花","鸡蛋","开心果","生蚝","西兰花"};
+        elements = new String[]{"维生素A","锌","维生素B12"};
+        elementLinearLayout = (LinearLayout) findViewById(R.id.elementLinearLayout);
+        for (String element : elements){
+            View view = LayoutInflater.from(this).inflate(R.layout.v3_element_cell, null);
+            TextView elementTextView = (TextView)view.findViewById(R.id.elementTextView);
+            elementTextView.setText(element);
+            elementLinearLayout.addView(view);
+        }
+
+        recommentFoods = new ArrayList<Pair<String, String>>();
+        recommentFoods.add(new Pair<String, String>("鸡蛋","2个"));
+        recommentFoods.add(new Pair<String, String>("开心果","50克"));
+        recommentFoods.add(new Pair<String, String>("生蚝","2个"));
+        recommentFoods.add(new Pair<String, String>("西兰花","100克"));
+        recommentFoods.add(new Pair<String, String>("鸡蛋","2个"));
+        recommentFoods.add(new Pair<String, String>("开心果","50克"));
+        recommentFoods.add(new Pair<String, String>("生蚝","2个"));
+        recommentFoods.add(new Pair<String, String>("西兰花","100克"));
         recommendViewPager = (LinearLayout) findViewById(R.id.recommendViewPager);
-        for (String recommentFood : recommentFoods){
+        for (Pair<String, String> recommentFood : recommentFoods){
             View view = LayoutInflater.from(this).inflate(R.layout.v3_recomment_food_cell, null);
+            TextView foodNameTextView = (TextView) view.findViewById(R.id.foodNameTextView);
+            TextView foodCountTextView = (TextView) view.findViewById(R.id.foodCountTextView);
+            foodNameTextView.setText(recommentFood.first);
+            foodCountTextView.setText(recommentFood.second);
             recommendViewPager.addView(view);
         }
+
+        diseases = new String[]{"急性咽炎","关节炎","流行性感冒"};
+        diseaseLinearLayout = (LinearLayout) findViewById(R.id.diseaseLinearLayout);
+        for (String disease : diseases){
+            View view = LayoutInflater.from(this).inflate(R.layout.v3_disease_cell, null);
+            TextView diseaseTextView = (TextView)view.findViewById(R.id.diseaseTextView);
+            diseaseTextView.setText(disease);
+            diseaseLinearLayout.addView(view);
+        }
+
+        attentions = new String[]{"1. 少吃熏制，腌制，富含硝酸盐的食品","2. 避免大量饮酒，吸烟","3. 充分睡眠","4. 多饮水，保持室内空气流通"};
+        attentionLinearLayout = (LinearLayout) findViewById(R.id.attentionLinearLayout);
+        for (String attention : attentions){
+            View view = LayoutInflater.from(this).inflate(R.layout.v3_attention_cell, null);
+            TextView attentionTextView = (TextView)view.findViewById(R.id.attentionTextView);
+            attentionTextView.setText(attention);
+            attentionLinearLayout.addView(view);
+        }
+
     }
     
     class RecommentFoodAdapter extends FragmentPagerAdapter {
