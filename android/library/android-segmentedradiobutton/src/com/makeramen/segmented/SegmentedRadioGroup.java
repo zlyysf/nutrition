898e6b7544ef01d7adedd840aa5f7c6eba783c3a
zlyIos <lianyu.zhang+ios1@yasofon.com>
2013-12-27 17:31:40 +0800

    added custom attribute for SegmentedRadioGroup and implemented tabbed controls ui effect in foods by nutrient page

--- android/library/android-segmentedradiobutton/src/com/makeramen/segmented/SegmentedRadioGroup.java
+++ android/library/android-segmentedradiobutton/src/com/makeramen/segmented/SegmentedRadioGroup.java
@@ -16,19 +16,42 @@
 
 package com.makeramen.segmented;
 
+
+
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.RadioGroup;
 
 public class SegmentedRadioGroup extends RadioGroup {
+	
+	int m_segment_radio_left = com.makeramen.segmented.R.drawable.segment_radio_left;
+	int m_segment_radio_middle = com.makeramen.segmented.R.drawable.segment_radio_middle;
+	int m_segment_radio_right = com.makeramen.segmented.R.drawable.segment_radio_right;
+	int m_segment_button = com.makeramen.segmented.R.drawable.segment_button;
 
 	public SegmentedRadioGroup(Context context) {
 		super(context);
+		this.readStyleParameters(context, null);
 	}
 
 	public SegmentedRadioGroup(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		this.readStyleParameters(context, attrs);
 	}
+	
+	private void readStyleParameters(Context context, AttributeSet attributeSet) {
+        TypedArray a = context.obtainStyledAttributes(attributeSet, R.styleable.SegmentedRadioGroup);
+        try {
+        	m_segment_radio_left = a.getResourceId(R.styleable.SegmentedRadioGroup_segment_radio_left, m_segment_radio_left);
+        	m_segment_radio_middle = a.getResourceId(R.styleable.SegmentedRadioGroup_segment_radio_middle, m_segment_radio_middle);
+        	m_segment_radio_right = a.getResourceId(R.styleable.SegmentedRadioGroup_segment_radio_right, m_segment_radio_right);
+        	m_segment_button = a.getResourceId(R.styleable.SegmentedRadioGroup_segment_button, m_segment_button);
+        } finally {
+            a.recycle();
+        }
+    }
+	
 
 	@Override
 	protected void onFinishInflate() {
@@ -40,13 +63,13 @@ public class SegmentedRadioGroup extends RadioGroup {
 		int count = super.getChildCount();
 
 		if(count > 1){
-			super.getChildAt(0).setBackgroundResource(com.makeramen.segmented.R.drawable.segment_radio_left);
+			super.getChildAt(0).setBackgroundResource(m_segment_radio_left);
 			for(int i=1; i < count-1; i++){
-				super.getChildAt(i).setBackgroundResource(com.makeramen.segmented.R.drawable.segment_radio_middle);
+				super.getChildAt(i).setBackgroundResource(m_segment_radio_middle);
 			}
-			super.getChildAt(count-1).setBackgroundResource(com.makeramen.segmented.R.drawable.segment_radio_right);
+			super.getChildAt(count-1).setBackgroundResource(m_segment_radio_right);
 		}else if (count == 1){
-			super.getChildAt(0).setBackgroundResource(com.makeramen.segmented.R.drawable.segment_button);
+			super.getChildAt(0).setBackgroundResource(m_segment_button);
 		}
 	}
 }
