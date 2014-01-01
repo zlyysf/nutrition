eb1f554cc4650367b221fa3b52befdf9e7272ab8
zlyIos <lianyu.zhang+ios1@yasofon.com>
2014-01-01 18:58:57 +0800

    fixed a warning IntentReceiverLeaked

--- android/NutritionFoodGuide/src/com/lingzhimobile/nutritionfoodguide/v3/activity/V3ActivityIllness.java
+++ android/NutritionFoodGuide/src/com/lingzhimobile/nutritionfoodguide/v3/activity/V3ActivityIllness.java
@@ -47,6 +47,15 @@ public class V3ActivityIllness extends V3BaseActivity {
         setViewsContent();
     }
     
+    @Override
+    protected void onDestroy() {
+		//fix a warning . com.lingzhimobile.nutritionfoodguide.v3.activity.V3ActivityIllness has leaked IntentReceiver com.android.qualcomm.browsermanagement.BrowserManagement$1@41cd7790 that was originally registered here. Are you missing a call to unregisterReceiver()?
+    	//android.app.IntentReceiverLeaked: Activity com.lingzhimobile.nutritionfoodguide.v3.activity.V3ActivityIllness has leaked IntentReceiver com.android.qualcomm.browsermanagement.BrowserManagement$1@41cd7790 that was originally registered here. Are you missing a call to unregisterReceiver()?
+    	//solved by http://angrycode.cn/archives/476
+    	m_webView1.destroy();
+		super.onDestroy();
+	}
+    
     void initViewHandles(){
     	Button rightButton = (Button) findViewById(R.id.rightButton);
     	rightButton.setVisibility(View.GONE);
